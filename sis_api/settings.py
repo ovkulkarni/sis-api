f1e9eac263752365c5410b8ee8362857bca5e210
ovkulkarni <ovkulkarni@users.noreply.github.com>
2017-11-20 11:01:03 -0500

    add static files

--- sis_api/settings.py
+++ sis_api/settings.py
@@ -119,6 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
