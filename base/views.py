3d82c37e01c818cc54ed37be92e99a2029ec4c5a
ovkulkarni <ovkulkarni@users.noreply.github.com>
2017-11-20 20:06:51 -0500

    import redirect

--- base/views.py
+++ base/views.py
@@ -1,4 +1,5 @@
 from collections import OrderedDict
+from django.shortcuts import redirect
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
