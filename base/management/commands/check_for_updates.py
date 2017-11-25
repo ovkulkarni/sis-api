2081d1c4023b7ebd2cbd894525dc57e997e0057f
ovkulkarni <2019okulkarn@tjhsst.edu>
2017-11-25 18:58:56 -0500

    multithread and fix data notification

--- base/management/commands/check_for_updates.py
+++ base/management/commands/check_for_updates.py
@@ -2,34 +2,42 @@ from django.core.cache import cache
 from django.core.management.base import BaseCommand
 from base.helpers import get_quarter_grades
 from base.models import User
-from fcm_django.models import FCMDevice
+from multiprocessing import Pool
+from rest_framework.renderers import JSONRenderer
+from json import loads
 
 
 class FakeRequest(object):
     def __init__(self, user, password=None, force=False):
         self.user = user
-        self.session = {'password': password}
+        self.session = {}
+        if password:
+            self.session.update({'password': password})
         self.GET = {'force': force}
+
+
+def check_user_updates(user):
+    print("Checking for updates for {}".format(user))
+    devices = user.fcmdevice_set.filter(active=True)
+    key = "{}:{}".format(user.username, "Current")
+    cached = cache.get(key)
+    new_grades = get_quarter_grades(FakeRequest(user, force=True), "", "")
+    if cached:
+        for i in range(len(new_grades['courses'])):
+            course = new_grades['courses'][i]
+            for assignment in course['assignments']:
+                if assignment not in cached['courses'][i]['assignments']:
+                    for device in devices:
+                        device.send_message(title="Grade Posted: {}".format(course['name']),
+                                            body="{}: {}".format(assignment['name'], assignment['score']),
+                                            data=loads(JSONRenderer().render(course)))
+                        print('Sent notification to {}, device {}'.format(device.user.username, device.registration_id))
 
 
 class Command(BaseCommand):
     help = 'Runs notifications for updates in grades'
 
     def handle(self, *args, **options):
-        for user in User.objects.filter(encrypted_password__isnull=False):
-            self.stdout.write("Checking for updates for {}".format(user))
-            devices = FCMDevice.objects.filter(user=user)
-            key = "{}:{}".format(user.username, "Current")
-            cached = cache.get(key)
-            new_grades = get_quarter_grades(FakeRequest(user, force=True), "", "")
-            if cached:
-                for i in range(len(new_grades['courses'])):
-                    course = new_grades['courses'][i]
-                    for assignment in course['assignments']:
-                        if assignment not in cached['courses'][i]['assignments']:
-                            for device in devices:
-                                device.send_message(title="Grade Posted: {}".format(course['name']),
-                                                    body="{}: {}".format(assignment['name'], assignment['score']),
-                                                    data=course)
-                                print('Sent notification to {}, device {}'.format(device.user.username, device.registration_id))
+        with Pool(4) as pool:
+            pool.map(check_user_updates, User.objects.filter(encrypted_password__isnull=False))
         print('Successfully finished sending notifications')
